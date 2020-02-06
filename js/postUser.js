ac0891eaead984b7a159b07792eff56e2dd78041
constanzagra <riverosaconstanza@gmail.com>
2020-02-06 20:16:52 -0300

    postUser fixed

--- js/postUser.js
+++ js/postUser.js
@@ -1,56 +1,4 @@
-//MOSTRAR Y CERRAR MODAL ADD EMPLOYEE
-
-const addNewEmployeeButton = document.querySelector("#add-employee-button");
-const addEmployeeModal = document.querySelector("#form-add");
-addNewEmployeeButton.addEventListener("click", () =>{
-    addEmployeeModal.setAttribute("style", "display: block")
-});
-
-const closeAddEmployeeModal = document.querySelector("#close-add-modal");
-const cancelAddEmployeeModal = document.querySelector("#cancel-button")
-closeAddEmployeeModal.addEventListener("click", () =>{
-    addEmployeeModal.setAttribute("style", "display: none")
-});
-
-cancelAddEmployeeModal.addEventListener("click", () =>{
-    addEmployeeModal.setAttribute("style", "display: none")
-})
-
-const nameInput = document.querySelector("#input-name");
-const emailInput = document.querySelector("#input-email");
-const adressInput = document.querySelector("#input-adress");
-const numberInput = document.querySelector("#input-number");
-const employeesList = document.querySelector("#manage-employees");
-const submitButton = document.querySelector("#submit-button");
-
-const postUser = async () =>{
-    let fullName = nameInput.value;
-    let email = emailInput.value;    
-    let adress = adressInput.value;
-    let phone = numberInput.value;
-
-    let newEmployee = {
-        fullName,
-        email,
-        adress,
-        phone
-    }
-    try {
-        const res = await axios.post(baseUrl, newEmployee);
-        newEmployee = res.data
-        Users.push(newEmployee)
-        showEmployees();
-    }
-    catch (err){
-        handleError(err)
-    }
-
-};
-
-submitButton.addEventListener("click", postUser)
-/*
-const postUser = async () => {
-
+const postUser = async (fullname, email, address, phone) => {
     try {
         const res = await axios.post(`${baseUrl}`, {
             fullname,
@@ -60,9 +8,51 @@ const postUser = async () => {
         })
         newUser = res.data
         Users.push(newUser)
-        load();
+        showEmployees();
     }
     catch (err){
         handleError(err)
     }
-};*/
+};
+
+//MOSTRAR Y CERRAR MODAL ADD EMPLOYEE
+
+const addModal = () => {
+    const addEmployeeModal = document.querySelector("#form-add");
+    addEmployeeModal.setAttribute("style", "display: block")
+
+    const closeAddEmployeeModal = document.querySelector("#close-add-modal");
+    closeAddEmployeeModal.onclick = function () {
+        addEmployeeModal.setAttribute("style", "display: none")
+    }
+    event.preventDefault();
+    const cancelAddEmployeeModal = document.querySelector("#cancel-button")
+    cancelAddEmployeeModal.onclick = function () {
+        addEmployeeModal.setAttribute("style", "display: none")
+    }
+    
+}
+
+    
+
+    const addNewEmployeeButton = document.querySelector("#add-employee-button");
+    addNewEmployeeButton.addEventListener("click", () =>{
+    addModal();
+    });
+
+    const nameInput = document.querySelector("#input-name");
+    const emailInput = document.querySelector("#input-email");
+    const adressInput = document.querySelector("#input-adress");
+    const numberInput = document.querySelector("#input-number");
+    const submitButton = document.querySelector("#submit-button");
+
+
+    submitButton.addEventListener("click", ()=> {
+        event.preventDefault();
+        
+        postUser(nameInput.value, emailInput.value, adressInput.value, numberInput.value);
+        //PARA QUE AL AGREGAR EL MODAL SE VAYA
+        const addEmployeeModal = document.querySelector("#form-add");
+        addEmployeeModal.setAttribute("style", "display: none")
+
+    }) 
